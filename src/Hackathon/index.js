ab8f8de0338998bd51e5fd43666c8998fdad322c
Thomas Mirmo <thomas.mirmotahari@gmail.com>
2017-08-28 10:12:25 -0700

    Building our hackathon landing page

---
+++ src/Hackathon/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Hackathon extends Component {
+  render () {
+    return (
+      <div>
+        Hello World!
+      </div>
+    )
+  }
+}
+
+export default Hackathon
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
+// Components
+import App from './App'
+import Hackathon from './Hackathon'
+
+// Fonts
 import 'typeface-roboto'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+      <Route path="/hackathon" component={Hackathon} />
+    </div>
+  </Router>, document.getElementById('root'))
 registerServiceWorker()
