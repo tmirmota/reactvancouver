0f90fb69ec02abd08f97df2e2dfcb2611ce4f211
Thomas Mirmo <thomas.mirmotahari@gmail.com>
2017-09-11 22:49:46 -0700

    Removed material ui from layout and removed theme.js

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import stylesheet from 'styles/index.scss'
-import theme from 'styles/theme.js'
-
-import { MuiThemeProvider } from 'material-ui/styles'
 
 export default ({ children, title = 'React Vancouver' }) => (
   <div>
@@ -28,9 +25,14 @@ export default ({ children, title = 'React Vancouver' }) => (
       <script src="https://use.fontawesome.com/003c3f5408.js" />
 
       {/* Material Design Lite */}
-      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-      <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css" />
-      
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      />
+      <link
+        rel="stylesheet"
+        href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css"
+      />
 
       {/* Google Roboto Font */}
       <link
@@ -40,9 +42,9 @@ export default ({ children, title = 'React Vancouver' }) => (
     </Head>
     <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
 
-    <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>
+    {children}
 
     <footer />
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js" />
   </div>
 )
