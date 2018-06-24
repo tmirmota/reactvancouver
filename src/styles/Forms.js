8fecdeda61b91a20c6b7debeb62505bb27404ce9
Thomas Mirmotahari <thomas.mirmotahari@gmail.com>
2018-06-24 15:35:40 -0700

    Resolved merge conflicts

--- src/styles/Forms.js
+++ src/styles/Forms.js
@@ -1,11 +1,46 @@
 import { css } from 'emotion'
 import Layout from './Layout'
 import Colors from './Colors'
+import Typography from './Typography'
 
 export const label = css({
+  fontFamily: Typography.font.base,
   color: Colors.theme.primary,
   fontSize: Layout.calcSpace(2),
+  fontWeight: Typography.font.weight.bold,
   margin: `${Layout.calcSpace(2)} 0 ${Layout.calcSpace(1)} 0`,
 })
-export const placeholder = css({})
-export const text = css({})
+
+export const placeholder = css({
+  fontFamily: Typography.font.base,
+  color: Colors.grey.calc(70),
+  fontSize: Layout.calcSpace(2),
+  fontWeight: Typography.font.weight.regular,
+  margin: `${Layout.calcSpace(2)} 0 ${Layout.calcSpace(1)} 0`,
+})
+
+export const text = css({
+  fontFamily: Typography.font.base,
+  color: Colors.grey.calc(70),
+  fontSize: Layout.calcSpace(2),
+  fontWeight: Typography.font.weight.bold,
+})
+
+export const input = css(text, {
+  backgroundColor: 'transparent',
+  border: 'none',
+  boxSizing: 'border-box',
+  margin: `0 0 ${Layout.calcSpace(2)} 0`,
+  padding: `${Layout.calcSpace(2)} ${Layout.calcSpace(4)}`,
+  transition: `border-color 0.2s ease`,
+
+  ':disabled': {
+    backgroundColor: 'transparent',
+    cursor: 'not-allowed',
+  },
+})
+
+export default {
+  label,
+  placeholder,
+}
--- src/styles/Shadows.js
+++ src/styles/Shadows.js
@@ -1,3 +1,9 @@
 export const border = `1px solid rgba(0,0,0,0.05)`
 export const shadow = `0 6px 12px 0 rgba(0,0,0,0.05), 0 1px 2px 0 rgba(0,0,0,0.05)`
 export const halo = `0 6px 12px 0 rgba(255,255,255,0.1), 0 1px 2px 0 rgba(255,255,255,0.1)`
+
+export default {
+  border,
+  shadow,
+  halo,
+}
