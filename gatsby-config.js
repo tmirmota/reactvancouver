d3bb678025946ff56ae354b417375525870b9b2e
Thomas Mirmotahari <thomas.mirmotahari@gmail.com>
2018-06-24 11:10:28 -0700

    Updated config with resovle src and contentful plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: 'React Vancouver',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: '019o3pe3du31',
+        accessToken:
+          '5d11a0074d873bf55ecd8729a9afde231634ea2445b84d9c62dad3abbbd6ee88',
+      },
+    },
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-resolve-src',
+  ],
 }
