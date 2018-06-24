5e6c73dfbd572b46e884366015d917112c18d9ed
Eric Kim <bosung90@gmail.com>
2018-06-24 13:42:31 -0700

    temp comment out gatsby-source-contentful to remove plugins error

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,14 +3,14 @@ module.exports = {
     title: 'React Vancouver',
   },
   plugins: [
-    {
-      resolve: 'gatsby-source-contentful',
-      options: {
-        spaceId: '019o3pe3du31',
-        accessToken:
-          '5d11a0074d873bf55ecd8729a9afde231634ea2445b84d9c62dad3abbbd6ee88',
-      },
-    },
+    // {
+    //   resolve: 'gatsby-source-contentful',
+    //   options: {
+    //     spaceId: '019o3pe3du31',
+    //     accessToken:
+    //       '5d11a0074d873bf55ecd8729a9afde231634ea2445b84d9c62dad3abbbd6ee88',
+    //   },
+    // },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-resolve-src',
   ],
