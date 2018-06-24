c2f17e36b3703f0474d9766aae41201750bafe11
Thomas Mirmotahari <thomas.mirmotahari@gmail.com>
2018-06-24 10:53:07 -0700

    Added header links

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'React Vancouver',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+const styles = {
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  nav: {
+    display: 'flex',
+  },
+}
+
 const Header = ({ siteTitle }) => (
-  <div>
+  <header style={styles.header}>
     <div>
       <h1>
         <Link to="/">{siteTitle}</Link>
       </h1>
     </div>
-  </div>
+    <nav style={styles.nav}>
+      <Link to="/events">Events</Link>
+      <Link to="/speakers">Speakers</Link>
+      <Link to="/jobs">Jobs</Link>
+      <Link to="/">Get Involved</Link>
+    </nav>
+  </header>
 )
 
 export default Header
