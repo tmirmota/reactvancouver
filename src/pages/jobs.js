7fb6403bce3efd208147542a2a207f67388fa601
Adil Karim <adil@aakarim.com>
2018-07-03 16:55:27 +0100

    Consume contentful jobs in Jobs component

--- src/pages/jobs.js
+++ src/pages/jobs.js
@@ -1,25 +1,41 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
+import moment from 'moment'
+import { RVText, RVCard, RVBox } from 'components'
 
-const Job = ({ id, title }) => (
-  <Link to={`/job/${id}`}>
-    <h3>{title}</h3>
-  </Link>
+const Job = ({ id, title, companyName }) => (
+  <RVCard mb2>
+    <Link to={`/job/${id}`}>
+      <RVText tag='h3'>{title}</RVText>
+      <RVText>{companyName}</RVText>
+    </Link>
+  </RVCard>
 )
 
-const Jobs = ({ data }) => {
-  const jobs = data.allContentfulJobs.edges
+// An active job starts before now and ends after now
+const filterActiveJobs = jobs =>
+  jobs.filter(job => {
+    // Convert dates to moment
+    const startDate = moment(job.node.startDate)
+    const endDate = moment(job.node.endDate)
+    if (startDate.isBefore() && endDate.isAfter()) {
+      return job
+    }
+  })
 
-  if (jobs.length <= 0) {
-    return <div>No Jobs</div>
+const Jobs = ({ data }) => {
+  const activeJobs = filterActiveJobs(data.allContentfulJobs.edges)
+
+  if (activeJobs.length <= 0) {
+    return <RVBox>No Jobs</RVBox>
   }
 
   return (
-    <div>
-      <h2>Jobs</h2>
-      {jobs.map(({ node }) => <Job key={node.id} {...node} />)}
-    </div>
+    <RVBox>
+      <RVText tag='h2'>Jobs</RVText>
+      {activeJobs.map(({ node }) => <Job key={node.id} {...node} />)}
+    </RVBox>
   )
 }
 
@@ -36,6 +52,14 @@ export const query = graphql`
         node {
           id
           title
+          companyName
+          urlToJobApplication
+          startDate
+          endDate
+          location {
+            lon
+            lat
+          }
         }
       }
     }
