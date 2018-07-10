151f6cb03072bd33aef39cfb3f2289720308cded
Thomas Mirmotahari <thomas.mirmotahari@gmail.com>
2018-07-09 21:59:57 -0700

    Started to update RVBadge

--- src/components/RVBadge.js
+++ src/components/RVBadge.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import { RVBox } from 'components'
+import { Colors } from 'styles'
+
+const styles = {
+  background: Colors.theme.primary,
+  borderRadius: 20,
+  color: Colors.grey.white,
+  width: 'fit-content',
+}
 
 const RVBadge = props => {
-  const { ...other } = props
-  return <RVBox {...other} />
+  const { className: customClassName, ...other } = props
+  const className = [customClassName, styles]
+  return <RVBox className={className} px2 py1 {...other} />
 }
 
 export default RVBadge
