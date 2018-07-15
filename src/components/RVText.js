eb3fcdcc702f3bbf54ad590478cfe2594a0e2c32
Thomas Mirmotahari <thomas.mirmotahari@gmail.com>
2018-07-14 22:41:55 -0700

    Updated RVText component

--- src/components/RVText.js
+++ src/components/RVText.js
@@ -1,25 +1,6 @@
 import React from 'react'
 import { Typography } from 'styles'
 import { RVBox } from 'components'
-import classNames from 'classnames'
+import { injectStyles } from 'utils'
 
-export default class RVText extends React.Component {
-  render() {
-    const { className: classNameProp, ...props } = this.props
-
-    const restProps = {}
-    const componentClasses = []
-
-    Object.keys(props).map(key => {
-      if (props[key] === true && Typography[key]) {
-        componentClasses.push(Typography[key])
-      } else {
-        restProps[key] = props[key]
-      }
-    })
-
-    const className = classNames(classNameProp)
-
-    return <RVBox {...restProps} className={className} />
-  }
-}
+export default injectStyles(Typography)(RVBox)
