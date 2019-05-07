71901b4372291da1a1f5607cfdd674d6b8a5e6e3
Eric Kim <bosung90@gmail.com>
2019-05-07 11:06:06 -0700

    twitter verify message

---
+++ src/pages/twitterVerify.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default () => (
+  <div>
+    FOLLOWING MESSAGE IS FOR TWITTER:
+    <br /> eric@reactvancouver.com
+    <br /> Eric Kim
+    <br /> Twitter @VancouverReact (admin@reactvancouver.com)
+    <br /> https://www.meetup.com/ReactJS-Vancouver-Meetup/
+    <br /> IS THE OFFICIAL ORGANIZER OF REACT VANCOUVER
+  </div>
+)
